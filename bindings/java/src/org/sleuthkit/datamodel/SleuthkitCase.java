8b0cfd82b17a20d42aa744cbd8173d6e15bce6a7
Luis Nassif <lfcnassif@gmail.com>
2022-04-13 11:44:50 -0300

    check if DB is writeable & skip writing calls to open read only DBs

--- bindings/java/src/org/sleuthkit/datamodel/SleuthkitCase.java
+++ bindings/java/src/org/sleuthkit/datamodel/SleuthkitCase.java
@@ -443,13 +443,17 @@ public class SleuthkitCase {
 			initEncodingTypes(connection);
 			initCollectedStatusTypes(connection);
 			populateHasChildrenMap(true);
-			updateExaminers(connection);
+			//iped-patch
+			if (canWrite(dbPath))
+			    updateExaminers(connection);
 			initDBSchemaCreationVersion(connection);
 		} 
 
 		fileManager = new FileManager(this);
 		communicationsMgr = new CommunicationsManager(this);
-		timelineMgr = new TimelineManager(this);
+		//iped-patch
+		if (canWrite(dbPath))
+		    timelineMgr = new TimelineManager(this);
 		dbAccessManager = new CaseDbAccessManager(this);
 		taggingMgr = new TaggingManager(this);
 		scoringManager = new ScoringManager(this);
@@ -470,6 +474,19 @@ public class SleuthkitCase {
 	ContentStreamProvider getContentProvider() {
 		return this.contentProvider;
 	}
+	
+	private static boolean canWrite(String dbPath) {
+	    File file = new File(dbPath);
+        if (!file.exists()) {
+            return false;
+        }
+        try (FileOutputStream fos = new FileOutputStream(file, true)) {
+            return true;
+
+        } catch (IOException e) {
+            return false;
+        }
+    }
 
 	/**
 	 * Returns a set of core table names in the SleuthKit Case database.
