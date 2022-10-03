f841f4bb55d5ff0dc1587432479449d6a67bb5b0
Luis Nassif <lfcnassif@gmail.com>
2022-10-03 14:30:05 -0300

    sleuthkit#2765: handles S-1-5-17 (IIS Default Account)

--- bindings/java/src/org/sleuthkit/datamodel/WindowsAccountUtils.java
+++ bindings/java/src/org/sleuthkit/datamodel/WindowsAccountUtils.java
@@ -170,6 +170,7 @@ final class WindowsAccountUtils {
 	//  - We can assume and fill in SID from given account name, and vice versa.
 	//  - We map account names in foreign languages (some known set) to english names, for these well known accounts. 
 	private static final Map<String, WellKnownSidInfo> SPECIAL_SIDS_MAP =  ImmutableMap.<String, WellKnownSidInfo>builder() 
+			.put("S-1-5-17", new WellKnownSidInfo(true, "S-1-5", NTAUTHORITY_REALM_NAME, "IUSR", "IIS Default Account"))
 			.put("S-1-5-18", new WellKnownSidInfo(true, "S-1-5", NTAUTHORITY_REALM_NAME, "SYSTEM", "Local System Account"))
 			.put("S-1-5-19", new WellKnownSidInfo(true, "S-1-5", NTAUTHORITY_REALM_NAME, "LOCAL SERVICE", "Local Service Account"))
 			.put("S-1-5-20", new WellKnownSidInfo(true, "S-1-5", NTAUTHORITY_REALM_NAME, "NETWORK SERVICE", "Network Service Account"))
@@ -208,7 +209,7 @@ final class WindowsAccountUtils {
 	// Input names provided by client are first mapped to english before lookup or insert. 
 	private static final Map<String, String> REALM_NAME_TO_ENGLISH_MAP =  ImmutableMap.<String, String>builder() 
 			.put("NT AUTHORITY", NTAUTHORITY_REALM_NAME)	// to facilitate a quick hit on the english name
-			.put("NT-AUTORITÄT", NTAUTHORITY_REALM_NAME)
+			.put("NT-AUTORITï¿½T", NTAUTHORITY_REALM_NAME)
 			.put("AUTORITE NT", NTAUTHORITY_REALM_NAME)
 			.put("NT INSTANS", NTAUTHORITY_REALM_NAME)
 			.build();
@@ -218,7 +219,7 @@ final class WindowsAccountUtils {
 	// Input names provided by client are first mapped to english before lookup or insert. 
 	private static final Map<String, String> LOGINNAME_TO_ENGLISH_MAP =  ImmutableMap.<String, String>builder() 
 			.put("SYSTEM", "SYSTEM")	// to facilitate a quick hit on the english name
-			.put("SYSTÈME", "SYSTEM")
+			.put("SYSTï¿½ME", "SYSTEM")
 			
 			.put("LOCAL SERVICE", "LOCAL SERVICE")
 			.put("LOKALER DIENST", "LOCAL SERVICE")
@@ -228,8 +229,8 @@ final class WindowsAccountUtils {
 			
 			.put("NETWORK SERVICE", "NETWORK SERVICE")
 			.put("NETZWERKDIENST", "NETWORK SERVICE")
-			.put("NÄTVERKSTJÄNST", "NETWORK SERVICE")
-			.put("SERVICE RÉSEAU", "NETWORK SERVICE")
+			.put("Nï¿½TVERKSTJï¿½NST", "NETWORK SERVICE")
+			.put("SERVICE Rï¿½SEAU", "NETWORK SERVICE")
 			.put("SERVIZIO DI RETE", "NETWORK SERVICE")
 			.put("SERVICIO DE RED", "NETWORK SERVICE")
 			.build();
